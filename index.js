7937973b8b6722ece042fb822f13c2ad4492216d
Ajay KC <meajay64@gmail.com>
2023-10-14 23:37:35 +0900

    Commit build before deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(cors());
 // app.use(morgan("tiny"));
 // app.use(
@@ -63,7 +64,8 @@ let persons = [
 ];
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  // res.send("hello world");
+  res.send();
 });
 
 app.get("/api/persons", (req, res) => {
